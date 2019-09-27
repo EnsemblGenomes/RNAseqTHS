4883ad44ae9c29af64fc905439c13ce1580e495d
mrossello <mrossello@ebi.ac.uk>
2019-09-27 15:26:42 +0100

    small update

--- THP/MarkNewSub_conf.pm
+++ THP/MarkNewSub_conf.pm
@@ -5,37 +5,22 @@ use warnings;
 
 use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
 
-=pod
-sub default_options {
+sub pipeline_wide_parameters {
     my ($self) = @_;
     return {
-        %{ $self->SUPER::default_options() },
-
-        'pipeline_name' => 'species_start',
-    };
+        %{$self->SUPER::pipeline_wide_parameters},  # here we inherit anything from the base class
+	# Overide any defaults here
+#	'ignore_pipe' => 0
+    }
 }
-
-
-sub pipeline_create_commands {
-    my ($self) = @_;
-    return [
-        @{$self->SUPER::pipeline_create_commands},
-    ];
-}
-=cut
 
 sub pipeline_analyses {
     my ($self) = @_;
     return [
         {   -logic_name => 'ena_start',
             -module     => 'THP::EnaStart',
-#            -parameters => [ {
-#                'PIPERUN'  => 2,
-#            } ],
             -flow_into => {
-		1      => { 'flag_finished' => 
-			    { 'PIPERUN' => undef } #check all crams in AERUNS table for submission, not just a specific piperun
-		}, 
+                1 => [ 'flag_finished' ], 
             },
         },
 	{
